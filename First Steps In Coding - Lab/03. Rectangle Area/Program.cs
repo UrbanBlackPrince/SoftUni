01d995785affe9af3afd0d9242823686f2db96a4
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2021-12-30 20:36:37 +0200

    add

---
+++ First Steps In Coding - Lab/03. Rectangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double area = a * a;
+
+            Console.WriteLine(area);
+        }
+    }
+}
