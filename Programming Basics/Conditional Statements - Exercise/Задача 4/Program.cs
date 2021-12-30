d1fcd5b3acbba733eca8829006ef1a15bd1d5023
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2021-12-30 20:12:54 +0200

    add

---
+++ Programming Basics/Conditional Statements - Exercise/Задача 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Задача_44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string inputUnit = Console.ReadLine();
+            string exitUnit = Console.ReadLine();
+
+            double result = 0;
+
+            if (inputUnit == "mm" && exitUnit == "m")
+            {
+                result = number / 1000;
+
+            }
+
+            else if (inputUnit == "mm" && exitUnit == "cm")
+            {
+                result = number / 10;
+            }
+
+
+            else if (inputUnit == "cm" && exitUnit == "mm")
+            {
+                result = number * 10;
+
+            }
+
+            else if (inputUnit == "cm" && exitUnit == "m")
+            {
+                result = number / 100;
+            }
+
+            else if (inputUnit == "m" && exitUnit == "mm")
+            {
+                result = number * 1000;
+            }
+
+
+            else if (inputUnit == "m" && exitUnit == "cm")
+            {
+                result = number * 100;
+            }
+
+
+            Console.WriteLine($"{result:f3}");
+
+        }
+    }
+}
