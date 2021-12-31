4d2d134157115222e4ab536224dc0065159a3c1a
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2021-12-31 15:34:33 +0200

    add

---
+++ Programming Basics C#/Conditional Statements - Exercise/01. Sum Seconds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalTime = 0;
+
+            totalTime += int.Parse(Console.ReadLine());
+            totalTime += int.Parse(Console.ReadLine());
+            totalTime += int.Parse(Console.ReadLine());
+
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            Console.WriteLine($"{minutes}:{seconds:d2}");
+
+        }
+    }
+}
