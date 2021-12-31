d68d34e050aac97329775060abe34715840e56a5
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2021-12-31 14:36:36 +0200

    add

---
+++ While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int numMin = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int currentNumber = int.Parse(input);
+
+                if (currentNumber < numMin)
+                {
+                    numMin = currentNumber;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{numMin}");
+        }
+    }
+}
