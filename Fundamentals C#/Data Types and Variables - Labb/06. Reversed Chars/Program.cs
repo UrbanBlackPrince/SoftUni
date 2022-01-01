7a2ec031e00bec611ec14af395fc6fe7af2b455b
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-01 18:36:08 +0200

    add

---
+++ Fundamentals C#/Data Types and Variables - Labb/06. Reversed Chars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = new char[3];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = char.Parse(Console.ReadLine());
+            }
+
+            arr = arr.Reverse().ToArray();
+
+            foreach (var t in arr)
+            {
+                Console.Write(t + " ");
+            }
+        }
+    }
+}
