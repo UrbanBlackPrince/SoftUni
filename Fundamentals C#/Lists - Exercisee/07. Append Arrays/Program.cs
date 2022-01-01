84fc7cbdf9b0efbc812e5de985a3ef4c6a31f6d3
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-01 19:55:45 +0200

    add

---
+++ Fundamentals C#/Lists - Exercisee/07. Append Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrays = Console.ReadLine()
+                 .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                 .Reverse()
+                 .ToList();
+
+            List<int> output = new List<int>();
+
+            foreach (var item in arrays)
+            {
+                int[] currentArray = item.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                foreach (var num in currentArray)
+                {
+                    output.Add(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', output));
+        }
+    }
+}
