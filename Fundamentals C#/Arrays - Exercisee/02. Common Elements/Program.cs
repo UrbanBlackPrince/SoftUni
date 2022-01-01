9c7cccc10877df4ec8d9c05a40fb46096b916bf5
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-01 19:00:14 +0200

    add

---
+++ Fundamentals C#/Arrays - Exercisee/02. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split();
+            string[] array2 = Console.ReadLine().Split();
+
+            foreach (var item2 in array2)
+            {
+                foreach (var item1 in array1)
+                {
+                    if (item1 == item2)
+                    {
+                        Console.Write(item1 + ' ');
+                    }
+                }
+            }
+        }
+    }
+}
