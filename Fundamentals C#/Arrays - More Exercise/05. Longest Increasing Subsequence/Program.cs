3fa0d59365b928019425634dc5a35b0551ce13d5
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-03 00:04:39 +0200

    add

---
+++ Fundamentals C#/Arrays - More Exercise/05. Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Longest_Increasing_Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] len = new int[nums.Length];
+            int[] prev = new int[nums.Length];
+            int maxLength = 0;
+            int maxIndex = -1;
+
+            for (int p = 0; p < nums.Length; p++)
+            {
+                len[p] = 1;
+                prev[p] = -1;
+
+                for (int left = 0; left < p; left++)
+                {
+                    if ((nums[left] < nums[p]) && (len[left] + 1 > len[p]))
+                    {
+                        len[p] = len[left] + 1;
+                        prev[p] = left;
+                    }
+                }
+
+                if (len[p] > maxLength)
+                {
+                    maxLength = len[p];
+                    maxIndex = p;
+                }
+            }
+
+           
+            int[] output = new int[maxLength];
+            int currentIndex = maxIndex;
+
+            for (int i = output.Length - 1; i >= 0; i--)
+            {
+                output[i] = nums[currentIndex];
+                currentIndex = prev[currentIndex];
+            }
+
+            Console.WriteLine(string.Join(' ', output));
+        }
+    }
+}
---
+++ Fundamentals C#/Arrays - More Exercise/Exam/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string input = Console.ReadLine();
+
+            while(input != "end")
+            {
+                string[] tokens = input
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                int firstNum = 0;
+                int secondNum = 0;
+
+                switch (tokens[0])
+                {
+                    case "swap":
+                        {
+                            firstNum = int.Parse(tokens[1]);
+                            secondNum = int.Parse(tokens[2]);
+
+                            int currNum = numbers[firstNum];
+                            numbers[firstNum] = numbers[secondNum];
+                            numbers[secondNum] = currNum;
+                            break;
+                        }
+
+                    case "multiply":
+                        firstNum = int.Parse(tokens[1]);
+                        secondNum = int.Parse(tokens[2]);
+
+                        numbers[firstNum] *= numbers[secondNum];
+                        break;
+                    case "decrease":
+                        {
+                            for (int j = 0; j < numbers.Length; j++)
+                            {
+                                numbers[j] -= 1;
+                            }
+
+                            break;
+                        }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(",", numbers));
+
+        }
+    }
+}
