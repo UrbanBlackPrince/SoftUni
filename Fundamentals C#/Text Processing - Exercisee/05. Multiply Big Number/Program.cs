711f3969468a12fbd2b616ba58234beb0b672a70
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-02 21:55:19 +0200

    add

---
+++ Fundamentals C#/Text Processing - Exercisee/05. Multiply Big Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNumber = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+            if (secondNumber == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            List<string> result = new List<string>();
+
+            int memory = 0;
+            for (int i = firstNumber.Length - 1; i >= 0; i--)
+            {
+                int currentDigit = int.Parse(firstNumber[i].ToString()) * secondNumber + memory;
+                memory = currentDigit / 10;
+                currentDigit %= 10;
+                result.Add(currentDigit.ToString());
+            }
+
+            if (memory > 0)
+            {
+                result.Add(memory.ToString());
+            }
+
+            result.Reverse();
+            Console.WriteLine(string.Join("", result));
+        }
+    }
+}
