a68bdd6481eee9555dea9d5c5c74562b884f0d45
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-02 23:00:20 +0200

    add

---
+++ Programming Basics C#/Conditional Statements - More Exercise/07. Flower Shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07._Flower_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programming Basics C#/First Steps In Coding - More Exercise/07. Circle Area And Perimeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07._Circle_Area_And_Perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double calculatedArea = Math.PI * r * r;
+            double calculatedParameter = Math.PI * r * 2;
+
+            Console.WriteLine($"{calculatedArea:f2}");
+            Console.WriteLine($"{calculatedParameter:f2}");
+        }
+    }
+}
