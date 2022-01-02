7799ff92132ca5a0d01c688e18e07c9e5e5955f6
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-02 23:17:37 +0200

    add

---
+++ Programming Basics C#/Conditional Statements Advanced - More Exercise/09. Numbers From 1 To 10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09._Numbers_From_1_To_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n < 11)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+        }
+    }
+}
