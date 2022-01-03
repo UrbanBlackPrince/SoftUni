dd6f1eb07a7cfbcade6ed21ee06b7fb26ab60975
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-03 18:51:27 +0200

    add

---
+++ Advanced C#/Streams Files and Directories - Lab/2. LineNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _2._LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = Path.Combine("data", "input.txt");
+            using StreamReader sr = new StreamReader(inputFile);
+            var outputFile = Path.Combine("data", "output.txt");
+            using StreamWriter sw = new StreamWriter(outputFile);
+            int lineCounter = 1;
+
+            string input;
+            while ((input = sr.ReadLine()) != null)
+            {
+                sw.WriteLine($"{lineCounter}. {input}");
+                lineCounter++;
+            }
+        }
+    }
+}
