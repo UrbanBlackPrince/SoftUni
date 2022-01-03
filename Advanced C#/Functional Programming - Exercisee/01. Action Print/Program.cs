8aeb93cd7b26f2320de8ecfa4b4c519a6e84f76b
UrbanBlackPrice33 <urbanblackprince@gmail.com>
2022-01-03 22:44:29 +0200

    add

---
+++ Advanced C#/Functional Programming - Exercisee/01. Action Print/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = s => Console.WriteLine(s);
+
+            Console.ReadLine().Split().ToList().ForEach(print);
+        }
+    }
+}
