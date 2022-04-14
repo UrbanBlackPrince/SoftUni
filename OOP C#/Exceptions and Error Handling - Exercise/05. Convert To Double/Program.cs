036e03b79c11f8b69f0284b38210ce99d692e546
Gabriel Nikolov <90791613+UrbanBlackPrince33@users.noreply.github.com>
2022-04-14 15:52:00 +0300

    Add files via upload

---
+++ OOP C#/Exceptions and Error Handling - Exercise/05. Convert To Double/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Convert.ToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string input = Console.ReadLine();
+                Console.WriteLine(Convert.ToDouble(input));
+            }
+            catch (InvalidCastException ice)
+            {
+                Console.Write("Exception Thrown: {0}: {1}", ice.GetType(), ice.Message);
+                throw;
+            }
+            catch (FormatException fe)
+            {
+                Console.Write("Exception Thrown: {0}: {1}", fe.GetType(), fe.Message);
+                throw;
+            }
+            catch (OverflowException oe)
+            {
+                Console.Write("Exception Thrown: {0}: {1}", oe.GetType(), oe.Message);
+                throw;
+            }
+        }
+    }
+}
