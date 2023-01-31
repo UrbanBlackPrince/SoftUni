74212649335da3b2815be557ea45a800a472cb90
Gabriel Nikolov <90791613+UrbanBlackPrince33@users.noreply.github.com>
2023-01-31 22:01:29 +0200

    Add files via upload

---
+++ C# DB/Subqueries and Joins/5. Employees Without Project.sql
@@ -0,0 +1,5 @@
+SELECT TOP(3) e.[EmployeeID], e.[FirstName] 
+	FROM [Employees] AS e
+	LEFT JOIN [EmployeesProjects] AS ep ON e.[EmployeeID] = ep.[EmployeeID]
+	WHERE e.[EmployeeID] NOT IN (SELECT [EmployeeID] FROM [EmployeesProjects])
+	ORDER BY e.[EmployeeID]
